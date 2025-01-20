575eb18319319ce51cf9163f2ab55d38a9c94d2a
Thanh Nhat 野良 <128158789+thanhnhat23@users.noreply.github.com>
2025-01-20 15:45:50 +0700

    Delete Web_ver2 directory

--- Web_ver2/assets/JS/musics.js
+++
@@ -1,5 +0,0 @@
-const music = document.getElementById('background-music');
-document.body.addEventListener('mouseenter', () => {
-    music.play().catch(err => console.error('Không thể phát nhạc:', err));
-    music.volume = 0.2;
-});
